9aa86f82cc8e0eb7fa50c8e44792ab81a26aa1d8
Michael Le <mnle529@gmail.com>
2020-01-26 17:29:25 -0800

    cleaned up the code and able to call genHTML

--- index.js
+++ index.js
@@ -6,33 +6,8 @@ const inquirer = require("inquirer");
 const github = require("github-scraper");
 const pdf = require('html-pdf');
 
-// My array of objects: Colors
-const colors = {
-  green: {
-    wrapperBackground: "#E6E1C3",
-    headerBackground: "#C1C72C",
-    headerColor: "black",
-    photoBorderColor: "#black"
-  },
-  blue: {
-    wrapperBackground: "#5F64D3",
-    headerBackground: "#26175A",
-    headerColor: "white",
-    photoBorderColor: "#73448C"
-  },
-  pink: {
-    wrapperBackground: "#879CDF",
-    headerBackground: "#FF8374",
-    headerColor: "white",
-    photoBorderColor: "#FEE24C"
-  },
-  red: {
-    wrapperBackground: "#DE9967",
-    headerBackground: "#870603",
-    headerColor: "white",
-    photoBorderColor: "white"
-  }
-};
+const generateHTML = require("./generateHTML");
+// const generateHTML2 = require("./generateHTML2");
 
 let profileImage;
 let userName;
@@ -44,11 +19,8 @@ let numRepo;
 let numFollowers;
 let numGithubStars;
 let numUsersFollowing;
-
 // variable for company 
 let userCompany;
-// stores user favorite color
-let userFavColor;
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const appendFileAsync = util.promisify(fs.appendFile);
@@ -76,8 +48,12 @@ function promptUser() {
       color,
     }) {
       const queryURL = `https://api.github.com/users/${username}`;
-      // console.log(username);
-      // console.log(color);
+      console.log(username);
+      console.log(color);
+
+      const html = generateHTML(color);
+      // console.log(html);
+      writeFileAsync("index.html", html);
 
       userFavColor = color;
 
@@ -100,8 +76,9 @@ function promptUser() {
         // });
 
         github(username, function (err, data) {
-          const html = generateHTML2(data.stars);
+          const html = generateHTML2(data.userInfo);
           return appendFileAsync("index.html", html);
+
         });
 
       })
@@ -121,155 +98,7 @@ function promptUser() {
     });
 }
 
-function generateHTML(data) {
-  return `<!DOCTYPE html>
-  <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-           background-color: ${colors[userFavColor].wrapperBackground};
-           padding-top: 100px;
-           }
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'Cabin', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'BioRhyme', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: ${colors[userFavColor].headerBackground};
-           color: ${colors[userFavColor].headerColor};
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid ${colors[userFavColor].photoBorderColor};
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-           }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
-           .container {
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
-
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: ${colors[userFavColor].headerBackground};
-             color: ${colors[userFavColor].headerColor};
-             margin: 20px;
-           }
-
-           .col {
-           flex: 1;
-           text-align: center;
-           }
-
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-        </style>`
-}
-
-function generateHTML2(stars) {
+function generateHTML2(userInfo) {
   return `
       </head>
       <body>
@@ -317,7 +146,7 @@ function generateHTML2(stars) {
               <div class="card col">
                 <h2>GitHub Stars</h2>
                 <!--arbitrary value-->
-                <h3>${stars}</h3>
+                <h3>${userInfo}</h3>
               </div>
               <div class="card col">
                 <h2>Following</h2>
@@ -331,12 +160,5 @@ function generateHTML2(stars) {
     </html>`
 }
 
-promptUser()
-  .then(function (answers) {
-    const html = generateHTML(answers);
-    return writeFileAsync("index.html", html);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+promptUser();
 
